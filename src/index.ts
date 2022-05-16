89cdfd521e5167a0b07515defbf60bd5f677fe3b
Jesper Kristensen <cableman@linuxdev.dk>
2022-05-16 19:27:30 +0200

    SERV-497: Add types packages for yargs and node

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,8 @@
 
 'use strict';
 
-const yargs = require('yargs/yargs')
-const {hideBin} = require('yargs/helpers')
+import yargs from "yargs";
+import {hideBin} from "yargs/helpers"
 
 const argv = yargs(hideBin(process.argv))
     .commandDir('commands')
